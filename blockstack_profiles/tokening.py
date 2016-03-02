d449c42fb0df1c694e9b107cc730f50e9cbaf30b
Ryan Shea <ryan@shea.io>
2016-03-02 17:03:20 -0500

    implement token signing, token validation and profile recovery functions

--- blockstack_profiles/tokening.py
+++ blockstack_profiles/tokening.py
@@ -1,16 +1,16 @@
 import jwt
+import json
 import ecdsa
+import datetime
 from keychain import PrivateKeychain, PublicKeychain
-
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import serialization
+from pybitcoin import BitcoinPrivateKey, BitcoinPublicKey
 
 
-def sign_profile_tokens(profile_components, private_keychain,
+def sign_profile_tokens(profile_components, parent_private_key,
                         signing_algorithm = 'ES256K'):
-    if not isinstance(private_keychain, PrivateKeychain):
-        raise ValueError("private_keychain must be a valid PrivateKeychain object")
+    """ Function for iterating through a list of profile components and
+        signing separate individual profile tokens.
+    """
 
     if signing_algorithm == 'ES256K':
         signing_algorithm = 'ES256'
@@ -18,45 +18,80 @@ def sign_profile_tokens(profile_components, private_keychain,
         raise ValueError("Unsupported signing algorithm")
 
     token_records = []
+    current_time = datetime.datetime.now()
 
     for profile_component in profile_components:
-        private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
-
-        pem_private_key = private_key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.PKCS8,
-            encryption_algorithm=serialization.NoEncryption())
-        pem_public_key = private_key.public_key().public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        private_key = BitcoinPrivateKey(parent_private_key)
+        public_key = private_key.public_key()
 
         payload = {
-            "claim": {},
+            "claim": profile_component,
             "subject": {
-                "publicKey": ""
+                "publicKey": public_key.to_hex()
             },
-            "issuedAt": "",
-            "expiresAt": ""
+            "issuedAt": current_time.isoformat(),
+            "expiresAt": current_time.replace(current_time.year + 1).isoformat()
         }
 
-        token = jwt.encode(payload, pem_private_key, algorithm=signing_algorithm)
-        decoded_token = jwt.decode(token, pem_public_key, algorithms=[signing_algorithm])
+        token = jwt.encode(
+            payload, private_key.to_pem(), algorithm=signing_algorithm)
+        decoded_token = jwt.decode(
+            token, public_key.to_pem(), algorithms=[signing_algorithm])
 
         token_record = {
             "token": token,
-            "data": decoded_token,
-            "publicKey": "",
-            "parentPublicKey": "",
-            "derivationEntropy": "",
+            "decoded_token": decoded_token,
+            "publicKey": public_key.to_hex(),
+            "parentPublicKey": public_key.to_hex(),
             "encrypted": False
         }
         token_records.append(token_record)
 
     return token_records
 
-def validate_token_record(token_record, public_keychain):
-    return None
 
-def get_profile_from_tokens(token_records, public_keychain):
-    return None
+def validate_token_record(token_record, parent_public_key,
+                          signing_algorithm = 'ES256'):
+    """ A function for validating an individual token record and extracting
+        the decoded token.
+    """
 
+    if not ("token" in token_record and "publicKey" in token_record and \
+            "parentPublicKey" in token_record):
+        raise ValueError("Invalid token record")
+
+    token = token_record["token"]
+
+    public_key = BitcoinPublicKey(parent_public_key)
+
+    decoded_token = jwt.decode(
+        token, public_key.to_pem(), algorithms=[signing_algorithm])
+
+    decoded_token = json.loads(json.dumps(decoded_token))
+
+    if "subject" not in decoded_token and "publicKey" not in decoded_token["subject"]:
+        raise ValueError("Invalid decoded token")
+    if "claim" not in decoded_token:
+        raise ValueError("Invalid decoded token")
+
+    if token_record["publicKey"] == token_record["parentPublicKey"]:
+        if token_record["publicKey"] != decoded_token["subject"]["publicKey"]:
+            raise ValueError("Token's public key doesn't match")
+    else:
+        raise ValueError("Verification of tokens signed with keychains is not yet supported")
+
+    return decoded_token
+
+
+def get_profile_from_tokens(token_records, parent_public_key):
+    """ A function for extracting a profile from a list of tokens.
+    """
+
+    profile = {}
+
+    for token_record in token_records:
+        decoded_token = validate_token_record(token_record, parent_public_key)
+        claim = decoded_token["claim"]
+        profile.update(claim)
+
+    return profile
--- unit_tests.py
+++ unit_tests.py
@@ -3,25 +3,38 @@ import traceback
 import unittest
 from test import test_support
 from keychain import PrivateKeychain, PublicKeychain
+from pybitcoin import BitcoinPrivateKey, BitcoinPublicKey
 from blockstack_profiles import sign_profile_tokens, validate_token_record, \
     get_profile_from_tokens
 
 
 class TokeningTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.master_private_key = BitcoinPrivateKey()
 
     def tearDown(self):
         pass
 
     def test_basic_tokening(self):
         profile_components = [
-            {"name": "Ryan Shea"}
+            {"name": "Ryan Shea"},
+            {"birthDate": "1990-01-01"}
         ]
-        private_keychain = PrivateKeychain()
-        profile_tokens = sign_profile_tokens(profile_components, private_keychain)
-        print json.dumps(profile_tokens, indent=2)
+        reference_profile = {
+            "name": "Ryan Shea", 
+            "birthDate": "1990-01-01"
+        }
+        # tokenize the profile
+        profile_tokens = sign_profile_tokens(
+            profile_components, self.master_private_key.to_hex())
+        #print json.dumps(profile_tokens, indent=2)
         self.assertTrue(isinstance(profile_tokens, list))
+        # recover the profile
+        profile = get_profile_from_tokens(
+            profile_tokens, self.master_private_key.public_key().to_hex())
+        #print json.dumps(profile, indent=2)
+        self.assertTrue(isinstance(profile, object))
+        self.assertEqual(profile, reference_profile)
 
 
 def test_main():
