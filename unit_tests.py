fc2e8a4fc0765288843d9f2076028d23b4542c3d
Ryan Shea <ryan@shea.io>
2016-03-07 18:42:57 -0500

    update and expand on unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -7,6 +7,96 @@ from pybitcoin import BitcoinPrivateKey, BitcoinPublicKey
 from blockstack_profiles import sign_profile_tokens, get_profile_from_tokens, \
     create_zone_file, get_person_from_legacy_format 
 
+reference_profiles = {
+    "legacy_1": {
+        "data_record": {
+            "avatar": {
+                "url": "https://s3.amazonaws.com/kd4/judecn"
+            },
+            "bio": "PhD student",
+            "bitcoin": {
+                "address": "17zf596xPvV8Z8ThbWHZHYQZEURSwebsKE"
+            },
+            "cover": {
+                "url": "https://s3.amazonaws.com/97p/gQZ.jpg"
+            },
+            "facebook": {
+                "proof": {
+                    "url": "https://facebook.com/sunspider/posts/674912239245011"
+                },
+                "username": "sunspider"
+            },
+            "github": {
+                "proof": {
+                    "url": "https://gist.github.com/jcnelson/70c02f80f8d4b0b8fc15"
+                },
+                "username": "jcnelson"
+            },
+            "location": {
+                "formatted": "Princeton University"
+            },
+            "name": {
+                "formatted": "Jude Nelson"
+            },
+            "twitter": {
+                "proof": {
+                    "url": "https://twitter.com/judecnelson/status/507374756291555328"
+                },
+                "username": "judecnelson"
+            },
+            "v": "0.2",
+            "website": "http://www.cs.princeton.edu/~jcnelson"
+        }
+    },
+    "legacy_2": {
+        "data_record": {
+            "avatar": {
+                "url": "https://s3.amazonaws.com/kd4/muneeb"
+            },
+            "bio": "Co-founder of Onename (YC S14), final-year PhD candidate at Princeton. Interested in distributed systems and blockchains.",
+            "bitcoin": {
+                "address": "1LNLCwtigWAvLkNakUK4jnmmvdVvmULeES"
+            },
+            "cover": {
+                "url": "https://s3.amazonaws.com/dx3/muneeb"
+            },
+            "facebook": {
+                "proof": {
+                    "url": "https://facebook.com/muneeb.ali/posts/10152524743274123"
+                },
+                "username": "muneeb.ali"
+            },
+            "github": {
+                "proof": {
+                    "url": "https://gist.github.com/muneeb-ali/9838362"
+                },
+                "username": "muneeb-ali"
+            },
+            "graph": {
+                "followee_count": 4,
+                "url": "https://s3.amazonaws.com/grph/muneeb"
+            },
+            "location": {
+                "formatted": "New York, NY"
+            },
+            "name": {
+                "formatted": "Muneeb Ali"
+            },
+            "pgp": {
+                "fingerprint": "9862A3FB338BE9EB6C6A5E05639C89272AFEC540",
+                "url": "http://muneebali.com/static/files/key.asc"
+            },
+            "twitter": {
+                "proof": {
+                    "url": "https://twitter.com/muneeb/status/483765788478689280"
+                },
+                "username": "muneeb"
+            },
+            "v": "0.2",
+            "website": "http://muneebali.com"
+        }
+    }
+}
 
 class TokeningTests(unittest.TestCase):
     def setUp(self):
@@ -27,12 +117,12 @@ class TokeningTests(unittest.TestCase):
         # tokenize the profile
         profile_token_records = sign_profile_tokens(
             profile_components, self.master_private_key.to_hex())
-        print json.dumps(profile_token_records, indent=2)
+        # print json.dumps(profile_token_records, indent=2)
         self.assertTrue(isinstance(profile_token_records, list))
         # recover the profile
         profile = get_profile_from_tokens(
             profile_token_records, self.master_private_key.public_key().to_hex())
-        print json.dumps(profile, indent=2)
+        # print json.dumps(profile, indent=2)
         self.assertTrue(isinstance(profile, object))
         self.assertEqual(profile, reference_profile)
 
@@ -48,110 +138,32 @@ class ZonefileTests(unittest.TestCase):
         origin = "naval.id"
         token_file_url = "https://mq9.s3.amazonaws.com/naval.id/profile.json"
         zone_file = create_zone_file(origin, token_file_url)
-        print json.dumps(zone_file, indent=2)
+        # print json.dumps(zone_file, indent=2)
         self.assertTrue(isinstance(zone_file, dict))
 
-    def test_profile_format_migration(self):
-        legacy_profile_1 = {
-            "data_record": {
-                "avatar": {
-                    "url": "https://s3.amazonaws.com/kd4/judecn"
-                },
-                "bio": "PhD student",
-                "bitcoin": {
-                    "address": "17zf596xPvV8Z8ThbWHZHYQZEURSwebsKE"
-                },
-                "cover": {
-                    "url": "https://s3.amazonaws.com/97p/gQZ.jpg"
-                },
-                "facebook": {
-                    "proof": {
-                        "url": "https://facebook.com/sunspider/posts/674912239245011"
-                    },
-                    "username": "sunspider"
-                },
-                "github": {
-                    "proof": {
-                        "url": "https://gist.github.com/jcnelson/70c02f80f8d4b0b8fc15"
-                    },
-                    "username": "jcnelson"
-                },
-                "location": {
-                    "formatted": "Princeton University"
-                },
-                "name": {
-                    "formatted": "Jude Nelson"
-                },
-                "twitter": {
-                    "proof": {
-                        "url": "https://twitter.com/judecnelson/status/507374756291555328"
-                    },
-                    "username": "judecnelson"
-                },
-                "v": "0.2",
-                "website": "http://www.cs.princeton.edu/~jcnelson"
-            }
-        }
+class LegacyFormatTests(unittest.TestCase):
+    def setUp(self):
+        pass
 
-        legacy_profile_2 = {
-            "data_record": {
-                "avatar": {
-                    "url": "https://s3.amazonaws.com/kd4/muneeb"
-                },
-                "bio": "Co-founder of Onename (YC S14), final-year PhD candidate at Princeton. Interested in distributed systems and blockchains.",
-                "bitcoin": {
-                    "address": "1LNLCwtigWAvLkNakUK4jnmmvdVvmULeES"
-                },
-                "cover": {
-                    "url": "https://s3.amazonaws.com/dx3/muneeb"
-                },
-                "facebook": {
-                    "proof": {
-                        "url": "https://facebook.com/muneeb.ali/posts/10152524743274123"
-                    },
-                    "username": "muneeb.ali"
-                },
-                "github": {
-                    "proof": {
-                        "url": "https://gist.github.com/muneeb-ali/9838362"
-                    },
-                    "username": "muneeb-ali"
-                },
-                "graph": {
-                    "followee_count": 4,
-                    "url": "https://s3.amazonaws.com/grph/muneeb"
-                },
-                "location": {
-                    "formatted": "New York, NY"
-                },
-                "name": {
-                    "formatted": "Muneeb Ali"
-                },
-                "pgp": {
-                    "fingerprint": "9862A3FB338BE9EB6C6A5E05639C89272AFEC540",
-                    "url": "http://muneebali.com/static/files/key.asc"
-                },
-                "twitter": {
-                    "proof": {
-                        "url": "https://twitter.com/muneeb/status/483765788478689280"
-                    },
-                    "username": "muneeb"
-                },
-                "v": "0.2",
-                "website": "http://muneebali.com"
-            }
-        }
-        zone_file_1 = get_person_from_legacy_format( legacy_profile_1 )
-        zone_file_2 = get_person_from_legacy_format( legacy_profile_2 )
-        print json.dumps(zone_file_1, indent=2, sort_keys=True)
-        print json.dumps(zone_file_2, indent=2, sort_keys=True)
-        self.assertTrue(isinstance(zone_file_1, dict))
-        self.assertTrue(isinstance(zone_file_2, dict))
+    def tearDown(self):
+        pass
+
+    def test_profile_format_migration(self):
+        zone_file = get_person_from_legacy_format(reference_profiles["legacy_1"])
+        # print json.dumps(zone_file, indent=2, sort_keys=True)
+        self.assertTrue(isinstance(zone_file, dict))
+
+    def test_profile_format_migration_2(self):
+        zone_file = get_person_from_legacy_format(reference_profiles["legacy_2"])
+        # print json.dumps(zone_file, indent=2, sort_keys=True)
+        self.assertTrue(isinstance(zone_file, dict))
+
 
 def test_main():
     test_support.run_unittest(
         TokeningTests,
-        ZonefileTests
+        ZonefileTests,
+        LegacyFormatTests
     )
 
 
