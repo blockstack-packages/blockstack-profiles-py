5437a58130b13b6fd44778b97d83f4707ec31be3
Ryan Shea <ryan@shea.io>
2016-03-10 19:50:50 -0500

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blockstack-schema',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/blockstack/blockstack-schema-py',
     license='MIT',
     author='Blockstack Developers',
