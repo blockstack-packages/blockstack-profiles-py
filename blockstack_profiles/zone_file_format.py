b3f479e153d313afe1cfafa984fde1bb9256714c
Jude Nelson <judecn@gmail.com>
2016-03-15 12:31:45 -0400

    Sync with zone_file format

--- blockstack_profiles/zone_file_format.py
+++ blockstack_profiles/zone_file_format.py
@@ -3,11 +3,15 @@ from zone_file import make_zone_file
 def make_zone_file_for_hosted_file(origin, token_file_url, ttl=3600):
     if "://" not in token_file_url:
         raise ValueError("Invalid token file URL")
-    records = [{
-        "name": "@", 
-        "class": "IN",
-        "type": "URI",
-        "data": token_file_url
-    }]
-    zone_file = make_zone_file(origin, ttl, records)
+    records = {
+        "URI" [
+            {
+                "name": "@",
+                "weight": 1,
+                "priority": 10,
+                "target": token_file_url
+            }
+        ]
+    }
+    zone_file = make_zone_file(records, origin=origin, ttl=ttl)
     return zone_file
