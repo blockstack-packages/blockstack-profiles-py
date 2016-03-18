ea2257f5f29e75c577680d7b7ebdcacc4989d92d
Ryan Shea <ryan@shea.io>
2016-03-17 20:04:43 -0400

    bump up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blockstack-profiles',
-    version='0.1.6',
+    version='0.2.0',
     url='https://github.com/blockstack/blockstack-profiles-py',
     license='MIT',
     author='Blockstack Developers',
