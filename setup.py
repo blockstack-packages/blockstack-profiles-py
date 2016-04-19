52c1614659d86a0ef04774b6e12d2261a942e1eb
Ryan Shea <ryan@shea.io>
2016-04-19 18:43:04 -0400

    increment version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blockstack-profiles',
-    version='0.3.5',
+    version='0.3.6',
     url='https://github.com/blockstack/blockstack-profiles-py',
     license='MIT',
     author='Blockstack Developers',
