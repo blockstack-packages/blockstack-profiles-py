9170121b20425e87e235d35c2ff6fcff946064d7
Jude Nelson <judecn@gmail.com>
2017-03-06 15:11:52 -0500

    revert: require 0.14.0 of blockstack-zones (since it's compatible with 0.14.1)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'cryptography>=1.2.3',
         'jsontokens>=0.0.2',
         'keylib>=0.0.5',
-        'blockstack-zones>=0.14.1',
+        'blockstack-zones>=0.14.0',
         'warlock>=1.3.0',
         'six>=1.4.1',
     ],
